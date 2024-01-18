432ee9f2ff2eb2b99abdb338e5471b9721f46483
Peter Kang <pxk7062@rit.edu>
2024-01-18 13:17:47 -0500

    Clean Vite check in

---
+++ EarthlyTeaTales/src/Navbar/Navbar.tsx
@@ -0,0 +1,13 @@
+const navigation = {
+  brand: { name: "Navbar", to: "/" },
+  links: [
+    { name: "Teas", to: "/" },
+    { name: "Notes", to: "/notes" },
+  ],
+};
+
+const Navbar = () => {
+  return <div></div>;
+};
+
+export default Navbar;

4e79da0e29e301311b6c53a2d1e9ec8f700b14fd
Peter Kang <pxk7062@rit.edu>
2025-01-29 21:45:52 -0500

    Planned out Tea Edits

--- EarthlyTeaTales/src/Page/TeaEdit/TeaEdit.tsx
+++ EarthlyTeaTales/src/Page/TeaEdit/TeaEdit.tsx
@@ -2,7 +2,15 @@ import './TeaEdit.css';
 const TeaEdit = ()=>{
     return (
         <div>
-            tea edit
+            {/* Image url*/}
+            {/* Name 64 chars*/}
+            {/* Overall rating out of 5*/}
+            {/* Tags 5 tags, drop down and button for adding*/}
+            {/* Description 900 chars */}
+            {/* Up to 7 repeats
+                Steep # and temperature in F (repeat up to 20th)
+                Review
+            */}
         </div>
     )
 }

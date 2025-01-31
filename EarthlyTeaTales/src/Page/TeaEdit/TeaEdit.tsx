0c64f6f81f0e490f026f61888dacab9902fd1398
Peter-Kang <pkang93@gmail.com>
2025-01-31 12:44:40 -0500

    added elements for steeps section

--- EarthlyTeaTales/src/Page/TeaEdit/TeaEdit.tsx
+++ EarthlyTeaTales/src/Page/TeaEdit/TeaEdit.tsx
@@ -5,8 +5,10 @@ const TeaEdit = ()=>{
     return (
         <div>
             <div>{/* Image url*/}
-                <label>Image</label>
-                <image></image>
+                <label>Image:</label>
+                <div>
+                    <img width="150px" src="https://happyearthtea.com/cdn/shop/files/IMG_3131_374x374.jpg?v=1711121609"></img>
+                </div>
                 <input type="file"></input>
             </div>
             <div> {/* Name 20 chars*/}
@@ -46,6 +48,97 @@ const TeaEdit = ()=>{
             <div>{/* Steeping notes Up to 7 repeats
                 Steep # and temperature in F (repeat up to 20th)
                 Review*/}
+                <div> {/* Gong Fu*/}
+                    <div>
+                        <label>Name: </label>
+                        <input type="text" maxLength={64} value="GongFu"></input>
+                    </div>
+                    <div>
+                        <label>Description</label>
+                        <textarea> </textarea>
+                    </div>
+                    <div>
+                        <div> {/*Steep 1*/}
+                            <label>Steep 1</label>
+                            <div>
+                                <label>Temperature</label>
+                                <input type="text" maxLength={3} style={{width:"3ch"}}></input>F
+                            </div>
+                            <div>
+                                <label>Time</label>
+                                <input type="text" maxLength={3} style={{width:"3ch"}} ></input>seconds 
+                            </div>
+                        </div>
+                        <div> {/*Steep 2*/}
+                            <label>Steep 2</label>
+                            <div>
+                                <label>Temperature</label>
+                                <input type="text" maxLength={3} style={{width:"3ch"}}></input>F
+                            </div>
+                            <div>
+                                <label>Time</label>
+                                <input type="text" maxLength={3} style={{width:"3ch"}}></input>seconds 
+                            </div>
+                        </div>
+                        <div> {/*Steep 3*/}
+                            <label>Steep 3</label>
+                            <div>
+                                <label>Temperature</label>
+                                <input type="text" maxLength={3} style={{width:"3ch"}}></input>F
+                            </div>
+                            <div>
+                                <label>Time</label>
+                                <input type="text" maxLength={3} style={{width:"3ch"}}></input>seconds 
+                            </div>
+                        </div>
+                        <div> {/*Steep 4*/}
+                            <label>Steep 4</label>
+                            <div>
+                                <label>Temperature</label>
+                                <input type="text" maxLength={3} style={{width:"3ch"}}></input>F
+                            </div>
+                            <div>
+                                <label>Time</label>
+                                <input type="text" maxLength={3} style={{width:"3ch"}}></input>seconds 
+                            </div>
+                        </div>
+                        <div> {/*Steep 5*/}
+                            <label>Steep 5</label>
+                            <div>
+                                <label>Temperature</label>
+                                <input type="text" maxLength={3} style={{width:"3ch"}}></input>F
+                            </div>
+                            <div>
+                                <label>Time</label>
+                                <input type="text" maxLength={3} style={{width:"3ch"}}></input>seconds 
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div> {/* Western */}
+                    <div>
+                        <label>Name:</label>
+                        <input type="text" maxLength={64} value="Western"></input>
+                    </div>
+                    <div>
+                        <label>Description:</label>
+                        <textarea> </textarea>
+                    </div>
+                    <div>
+                        <div> {/*Steep 1*/}
+                            <label>Steep 1</label>
+                            <div>
+                                <label>Temperature</label>
+                                <input type="text" maxLength={3} style={{width:"3ch"}}></input>F
+                            </div>
+                            <div>
+                                <label>Time</label>
+                                <input type="text" maxLength={3} style={{width:"3ch"}} ></input>seconds 
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
